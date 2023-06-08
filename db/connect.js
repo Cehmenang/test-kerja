c748af0115a5d43d9f8fc4aaa3db1bccc9b96ffe
cehmenang <heaven.cehwin12@gmail.com>
2023-06-08 10:38:09 +0700

    Memasukkan URI MongoDB

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
+const mongoURI = `mongodb+srv://Cehmenang:aOMmmXqRk601FEVP@cehmenang.0fs07tp.mongodb.net/Dummy?retryWrites=true&w=majority`
 
 export default async function(){
     try{
         mongoose.set('strictQuery', false)
-        return await mongoose.connect(process.env.MONGO_URI)
+        return await mongoose.connect(process.env.MONGO_URI || mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     }catch(err){ 
         console.error(err.message)
         return process.exit(1)
