d2ba195cb4ae9b8b42ede00f8c7acd53f8e3218b
cehmenang <heaven.cehwin12@gmail.com>
2023-06-08 11:53:00 +0700

    Update MongoURI

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
-const mongoURI = `mongodb+srv://Cehmenang:aOMmmXqRk601FEVP@cehmenang.0fs07tp.mongodb.net/Dummy?retryWrites=true&w=majority`
+const mongoURI = "mongodb+srv://Cehmenang:FnvS4YAPF8vRT1dT@cehmenang.0fs07tp.mongodb.net/?retryWrites=true&w=majority"
 
 export default async function(){
     try{
-        mongoose.set('strictQuery', false)
-        return await mongoose.connect(process.env.MONGO_URI || mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.set('strictQuery', false)
+        return await mongoose.connect(process.env.MONGO_URI || mongoURI)
     }catch(err){ 
         console.error(err.message)
         return process.exit(1)
